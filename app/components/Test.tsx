ae8efe834f873b1968d9d9322fab7c39b20fdb62
Mubin Ansari <mubinansari910@gmail.com>
2022-10-06 23:21:27 +0530

    implement trpc@10 for app

--- app/components/Test.tsx
+++ app/components/Test.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import { trpc } from '../utils/trpc';
+import { trpc } from "../utils/trpc";
 
 const Test = () => {
-  const hello = trpc.useQuery(['hello', { text: 'client' }]);
+  const hello = trpc.hello.useQuery({ text: "anuj, trpc se bol raha hu" });
   if (!hello.data) return <Text>Loading...</Text>;
   return (
     <View>
-      <Text>{hello.data.greeting}</Text>
+      <Text>{hello.data.message}</Text>
     </View>
   );
 };
 
 export default Test;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+});
