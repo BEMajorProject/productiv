9bf99f5bda44f190d3f7bfa7288d56babfa0c939
Anuj <anujjoshi63@gmail.com>
2022-10-11 09:56:34 +0530

    fix TopBar

--- desktop/src/renderer/components/TopBar/TopBar.tsx
+++ desktop/src/renderer/components/TopBar/TopBar.tsx
@@ -1,8 +1,8 @@
 const TopBar = () => {
   return (
-    <div className="border-black flex justify-center items-center border-b border-collapse h-16 ">
-      <div className="flex justify-center items-center  h-full w-fit  ">
-        <div className="p-2 rounded-full mx-3 flex flex-col ">
+    <div className="border-black flex justify-center items-center border-b border-collapse h-16">
+      <div className="flex justify-center items-center  h-full w-fit ">
+        <div className="p-2 rounded-full mx-3 flex flex-col">
           <span className="-m-2">—</span>
           <span className="-m-2">—</span>
           <span className="-m-2">—</span>
@@ -11,7 +11,7 @@ const TopBar = () => {
       <div className="flex justify-center items-center  h-full w-1/5  font-semibold text-xl">
         productiv
       </div>
-      <div className="flex justify-center items-center  h-full w-3/5 ">
+      <div className="flex justify-center items-center  h-full w-3/5">
         <input
           type="text"
           name="search"
@@ -19,7 +19,7 @@ const TopBar = () => {
           className="p-2 rounded-xl border-black border w-full mx-5 my-2"
         />
       </div>
-      <div className="flex justify-end items-center h-full w-2/5  ">
+      <div className="flex justify-end items-center h-full w-2/5 ">
         <div className="flex justify-between px-4">
           <div className=" mx-2 flex justify-center items-center bg-gray-300 rounded-xl w-[32px] h-[32px]">
             a
